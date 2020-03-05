02f0eff25ff50e0356df8e5a8753e72940b9e5de
Daniel Redondo <danielredondosanchez@hotmail.com>
2020-03-05 08:18:19 +0100

    package_checked

---
+++ R/search_functions.R
@@ -0,0 +1,74 @@
+#' futbin_search searchs players in futbin.
+#'
+#' @export futbin_search
+#' @importFrom magrittr %>%
+#' @param name Optional. Name of the player. If leaved blank,
+#' it will report the 30 highest-rated players of the game.
+#' @param version Optional. Version of the card. It can be "Normal",
+#' "CL" (Champions League), "IF" (In-Form), "SIF" (Second In-Form), ...
+#' @param messages Optional. To show additional messages
+#' (webpage used and number of players found)
+#' @return A dataframe with all the players found searching for \code{name}
+#' and with version matching \code{version}
+#' @examples
+#' futbin_search("Lionel Messi")
+#' futbin_search("Lionel Messi", version = "Normal")
+#' futbin_search("Luka Modric", version = "IF")
+futbin_search <- function(name = "", version = NULL, messages = F) {
+  if (length(name) == 1) {
+
+    # Name transformation
+    name <- name %>%
+      tolower() %>%
+      gsub(pattern = " ", replacement = "+")
+
+    # URL creation
+    url <- paste0("https://www.futbin.com/20/players?page=1&search=", name)
+
+    # Web scraping
+    tabla <- xml2::read_html(url) %>%
+      rvest::html_nodes(xpath = "//table") %>%
+      magrittr::extract(3) %>%
+      rvest::html_table() %>%
+      magrittr::extract2(1)
+
+    # Table names
+    colnames(tabla) <- c(
+      "name", "rating", "position", "ver", "ps_price", "skills", "weak_foot", "work_rate", "pac",
+      "sho", "pas", "dri", "def", "phy", "hei", "popularity", "base_stats", "in_game_stats"
+    )
+
+    # Work rates (attack and defense)
+    tabla$work_rate_attack <- substr(tabla$work_rate, 0, 1)
+    tabla$work_rate_defense <- substr(tabla$work_rate, 5, 6)
+
+    # Weigth and height
+    tabla$wei <- substr(tabla$hei, regexpr("[(]", tabla$hei) + 1, regexpr("[k]", tabla$hei) - 1)
+    tabla$hei <- substr(tabla$hei, 1, 3)
+
+    # Fixing prices (1K = 1000, 1M = 1000000)
+    aux_k <- regexpr(pattern = "K", tabla$ps, fixed = TRUE)
+    aux_M <- regexpr(pattern = "M", tabla$ps, fixed = TRUE)
+    tabla$ps <- gsub("K", "", tabla$ps)
+    tabla$ps <- gsub("M", "", tabla$ps)
+    tabla$ps <- as.double(tabla$ps)
+    for (i in 1:nrow(tabla)) {
+      if (aux_k[i] != -1) tabla$ps[i] <- tabla$ps[i] * 1000
+      if (aux_M[i] != -1) tabla$ps[i] <- tabla$ps[i] * 1000000
+    }
+
+    # Filter version of the player
+    if (is.null(version) == FALSE) tabla <- subset(tabla, tabla$ver == version)
+
+    if (messages == T) print(paste0("Reading... ", url))
+    if (messages == T) print(paste0("Player(s) found: ", nrow(tabla)))
+
+    return(tabla)
+  } else {
+    # Recursive search
+    aux <- name[1]
+    name <- name[-1]
+    return(rbind(futbin_search(aux, version, messages), futbin_search(name, version, messages)))
+  }
+}
+
--- README.md
+++ README.md
@@ -1,5 +1,24 @@
-#futbin: R package
+# rfut
 
-## futbin_search
+rfut: Search prices for FIFA Ultimate Team players in Futbin webpage
 
-## futbin_plot
+### Installation
+
+This package is currently available only on GitHub. To install it, use the
+`devtools` package:
+
+```{r}
+library(devtools)
+install_github("danielredondo/rfut")
+```
+### Usage
+
+There's just one exported function in this package, `futbin_search`. It has the following parameters:
+
+- `name`
+
+- `version`
+
+- `messages`
+
+### Examples
