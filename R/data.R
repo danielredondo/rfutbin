22d192f2862550f8409d86479f8c9e345888fa20
Daniel Redondo <danielredondosanchez@hotmail.com>
2020-03-12 13:12:25 +0100

    new_data

---
+++ R/data.R
@@ -0,0 +1,63 @@
+#' Futbin database at 12th March 2020.
+#'
+#' A dataset containing the stats available on Futbin of Aston Villa players.
+#'
+#' Generated with:
+#' futbin_scrap(url = "https://www.futbin.com/players?page=1&club=2")
+#'
+#' @format A data frame with 21705 rows and 22 variables:
+#' \describe{
+#'   \item{name}{price, in US dollars}
+#'   \item{rating}{weight of the diamond, in carats}
+#'   \item{position}{weight of the diamond, in carats}
+#'   \item{ver}{weight of the diamond, in carats}
+#'   \item{ps_price}{weight of the diamond, in carats}
+#'   \item{skills}{weight of the diamond, in carats}
+#'   \item{weak_foot}{weight of the diamond, in carats}
+#'   \item{work_rate}{weight of the diamond, in carats}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   \item{carat}{weight of the diamond, in carats}
+#' }
+#' @source \url{http://www.futbin.com/}
+"aston_villa"
+
+
+
+#' Futbin database at 12th March 2020.
+#'
+#' A dataset containing the stats available on Futbin of FC Barcelona players.
+#'
+#' Generated with:
+#' futbin_scrap(url = "https://www.futbin.com/players?page=1&club=2&club=241")
+#'
+#' @format A data frame with 21705 rows and 22 variables:
+#' \describe{
+#'   \item{name}{price, in US dollars}
+#'   \item{rating}{weight of the diamond, in carats}
+#'   \item{position}{weight of the diamond, in carats}
+#'   \item{ver}{weight of the diamond, in carats}
+#'   \item{ps_price}{weight of the diamond, in carats}
+#'   \item{skills}{weight of the diamond, in carats}
+#'   \item{weak_foot}{weight of the diamond, in carats}
+#'   \item{work_rate}{weight of the diamond, in carats}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   \item{carat}{weight of the diamond, in carats}
+#' }
+#' @source \url{http://www.futbin.com/}
+"barcelona"
