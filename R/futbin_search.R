06d099b06a3be34025eeaa1fbbec0b611ec6a0d0
Daniel Redondo <danielredondosanchez@hotmail.com>
2020-03-12 11:41:13 +0100

    better_names

--- R/futbin_search.R
+++ R/futbin_search.R
@@ -1,7 +1,7 @@
 #' futbin_search searchs players in futbin.
 #'
 #' @export futbin_search
-#' @importFrom magrittr %>%
+#' @importFrom dplyr %>%
 #' @param name Optional. Vector with the names of the players. If not specified
 #' it will report the 30 highest-rated players of the game.
 #' @param version Optional. Version of the cards. Some options are "Normal",
@@ -13,6 +13,7 @@
 #' futbin_search(name = "Lionel Messi")
 #' futbin_search(name = c("Lionel Messi", "Griezmann"), version = "Normal")
 #' futbin_search(name = "Luka Modric", version = "IF", messages = TRUE)
+
 futbin_search <- function(name = "", version = NULL, messages = F) {
   if (length(name) == 1) {
 
@@ -33,28 +34,30 @@ futbin_search <- function(name = "", version = NULL, messages = F) {
 
     # Table names
     colnames(tabla) <- c(
-      "name", "rating", "position", "version", "ps_price", "skills", "weak_foot", "work_rate",
+      "name", "rating", "position", "version", "price", "skills", "weak_foot", "work_rate",
       "pac", "sho", "pas", "dri", "def", "phy",
       "hei", "popularity", "base_stats", "in_game_stats"
     )
 
     # Work rates (attack and defense)
-    tabla$work_rate_attack <- substr(tabla$work_rate, 0, 1)
-    tabla$work_rate_defense <- substr(tabla$work_rate, 5, 6)
+    tabla$wr_attack <- substr(tabla$work_rate, 0, 1)
+    tabla$wr_defense <- substr(tabla$work_rate, 5, 6)
+    tabla <- tabla[ , -which(names(tabla) == "work_rate")]
 
     # Weigth and height
     tabla$wei <- substr(tabla$hei, regexpr("[(]", tabla$hei) + 1, regexpr("[k]", tabla$hei) - 1)
     tabla$hei <- substr(tabla$hei, 1, 3)
 
     # Fixing prices (1K = 1000, 1M = 1000000)
-    aux_k <- regexpr(pattern = "K", tabla$ps, fixed = TRUE)
-    aux_M <- regexpr(pattern = "M", tabla$ps, fixed = TRUE)
-    tabla$ps <- gsub("K", "", tabla$ps)
-    tabla$ps <- gsub("M", "", tabla$ps)
-    tabla$ps <- as.double(tabla$ps)
+    aux_k <- regexpr(pattern = "K", tabla$price, fixed = TRUE)
+    aux_M <- regexpr(pattern = "M", tabla$price, fixed = TRUE)
+    tabla$price <- tabla$price
+    tabla$price <- gsub("K", "", tabla$price)
+    tabla$price <- gsub("M", "", tabla$price)
+    tabla$price <- as.double(tabla$price)
     for (i in 1:nrow(tabla)) {
-      if (aux_k[i] != -1) tabla$ps[i] <- tabla$ps[i] * 1000
-      if (aux_M[i] != -1) tabla$ps[i] <- tabla$ps[i] * 1000000
+      if (aux_k[i] != -1) tabla$price[i] <- tabla$price[i] * 1000
+      if (aux_M[i] != -1) tabla$price[i] <- tabla$price[i] * 1000000
     }
 
     # Filter version of the player
